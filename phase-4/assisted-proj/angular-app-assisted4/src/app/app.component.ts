e433c0888b0a9dc6fc0b08e3860f782cc3bbb297
mahendar maheshuni <mahendar.maheshuni@7gmail.com>
2022-03-07 19:24:10 +0530

    assisted added

---
+++ phase-4/assisted-proj/angular-app-assisted4/src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  styleUrls: ['./app.component.css'],
+  template: `<product-list></product-list>`
+
+})
+export class AppComponent {
+  title = 'angular-app-Assisted4';
+}
